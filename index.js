9f97daf3c7bb6aa2dacc3a0204682773fb493006
NITISH03061999 <nitishdhaka162@gmail.com>
2025-03-22 23:23:45 +0530

    Fixed MongoDB connection issue

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 mongoDB();
-app.use(cors());
+app.use(cors({
+  origin: "https://food-deliverry-app-frontend.vercel.app/", // Your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.get("/", (req, res) => {
   res.send("HelloWorld");
 });
